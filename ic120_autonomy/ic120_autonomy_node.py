68b891df5cb247090525cc2ae410ce00639d6dc6
Ryoya SATO <satoryoya1012711@gmail.com>
2026-06-12 11:31:54 +0900

    First commit

---
+++ ic120_autonomy/ic120_autonomy_node.py
@@ -0,0 +1,6 @@
+def main():
+    print('Hi from ic120_autonomy.')
+
+
+if __name__ == '__main__':
+    main()
---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import find_packages, setup
+
+package_name = 'ic120_autonomy'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='zx120',
+    maintainer_email='satoryoya1012711@gmail.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+            'ic120_autonomy_node = ic120_autonomy.ic120_autonomy_node:main'
+        ],
+    },
+)
